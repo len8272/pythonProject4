5255a66e93a881594b8cc66849ca37449a5adf83
Elena Dorokhina <56249089+len8272@users.noreply.github.com>
2021-11-03 21:59:39 +0500

    Delete script.py

--- script.py
+++
@@ -1,7 +0,0 @@
-1 1 2 3 5 8 13 21 34 55
-string = input("Введите числа через пробел:")
-
-list_of_strings = string.split() # список строковых представлений чисел
-list_of_numbers = list(map(int, list_of_strings)) # список чисел
-
-print(sum(list_of_numbers[::3])) # sum() вычисляет сумму элементов списка
